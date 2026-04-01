d76d7bfc7593b94e4a98b1788ede7b98b31684f6
nitint-420 <nitinthakarenvt@email.com>
2026-04-02 04:59:39 +0530

    fix: add outputFileTracingRoot for prisma binary

--- apps/dashboard/next.config.js
+++ apps/dashboard/next.config.js
@@ -1,7 +1,11 @@
+const path = require("path");
+
 const nextConfig = {
   transpilePackages: ["@ecom/ui", "@ecom/utils", "@ecom/database"],
   experimental: {
     serverComponentsExternalPackages: ["@prisma/client", "prisma"],
   },
+  outputFileTracingRoot: path.join(__dirname, "../../"),
 };
+
 module.exports = nextConfig;
